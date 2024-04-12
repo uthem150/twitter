49a4a6c4bf3039eefc4b3e89f74a38f6556210d4
uthem150 <uthem150@gmail.com>
2024-04-12 21:43:25 +0900

    firebase 설치 및 프로젝트 생성

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC132Lyc05XtHEPOyqp_GMbWYLUTf--cvQ",
+  authDomain: "twitter-51b11.firebaseapp.com",
+  projectId: "twitter-51b11",
+  storageBucket: "twitter-51b11.appspot.com",
+  messagingSenderId: "694978790197",
+  appId: "1:694978790197:web:27d870c83c0d1a208d827c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
