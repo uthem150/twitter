0052b881ed451543a79bcb7f415fc56e11c89810
uthem150 <uthem150@gmail.com>
2024-04-16 22:20:40 +0900

    컴포넌트들 공유 라이브러리로 관리

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,64 +1,16 @@
-import { styled } from "styled-components";
 import { useState } from "react";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from "../firebase";
 import { Link, useNavigate } from "react-router-dom";
 import { FirebaseError } from "firebase/app";
-
-// const errors = {
-//   "auth/email-already-in-use" : "That email already exist"
-// }
-
-const Wrapper = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 420px;
-  padding: 50px 0px;
-`;
-
-const Title = styled.h1`
-  font-size: 42px;
-`;
-
-const Form = styled.form`
-  margin-top: 50px;
-  margin-bottom: 10px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  width: 100%;
-`;
-
-const Input = styled.input`
-  padding: 10px 20px;
-  border-radius: 50px;
-  border: none;
-  width: 100%;
-  font-size: 16px;
-
-  // type이 submit이면, cursor을 포인터로 변경
-  &[type="submit"] {
-    cursor: pointer;
-    // hover상황에서는 버튼 색을 변경
-    &:hover {
-      opacity: 0.8;
-    }
-  }
-`;
-
-const Error = styled.span`
-  font-weight: 600;
-  color: tomato;
-`;
-
-const Switcher = styled.span`
-  margin-top: 20px;
-  a {
-    color: #1d9bf0;
-  }
-`;
+import {
+  Form,
+  Error,
+  Input,
+  Switcher,
+  Title,
+  Wrapper,
+} from "../components/auth-components";
 
 export default function CreateAccount() {
   const [isLoading, setLoading] = useState(false);
