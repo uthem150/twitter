7d2526f2f08e70a751c25aaf5857d64c8db7c654
uthem150 <uthem150@gmail.com>
2024-04-18 23:12:48 +0900

    여러 state를 한 객체로 관리

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -16,33 +16,29 @@ import GithubButton from "../components/github-btn";
 export default function CreateAccount() {
   const [isLoading, setLoading] = useState(false);
 
-  // 받아온 name, email, password를 state에 저장
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  // error 상태를 별도로 관리
   const [error, setError] = useState("");
+
+  const [userInfo, setUserInfo] = useState({
+    //useState를 사용하여 name, email, password를 키로 하는 객체 하나로 상태를 관리
+    name: "",
+    email: "",
+    password: "",
+  });
   const navigate = useNavigate();
 
-  // React에서 입력 요소(input element)의 변경 이벤트를 처리하는 함수 (onChange라는 상수에 함수를 할당)
   // e는 변경 이벤트 객체이며, React.ChangeEvent<HTMLInputElement> 타입
   // (TypeScript 타입 어노테이션. e가 React의 입력 변경 이벤트 객체임을 명시, 이벤트가 HTML의 <input> 요소와 관련 있음을 나타냄)
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    // 이벤트 객체 e에서 target 객체를 구조 분해 할당하여 name과 value 속성을 추출 (target은 이벤트가 발생한 DOM 요소, 즉 입력 요소)
-    const {
-      target: { name, value },
-    } = e;
-    if (name === "name") {
-      setName(value);
-    } else if (name === "email") {
-      setEmail(value);
-    } else if (name === "password") {
-      setPassword(value);
-    }
+    const { name, value } = e.target; //e.target.name과 e.target.value를 통해 입력 필드의 이름과 현재 값을 추출
+    setUserInfo((prevState) => ({ ...prevState, [name]: value }));
   };
 
   // 폼 제출 이벤트가 발생할 때 호출
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault(); //이벤트의 기본 동작을 방지하는 메서드. 폼 제출 시 기본적으로 페이지가 새로고침되거나 서버로 데이터가 전송되는 것을 방지
+    const { name, email, password } = userInfo; // userInfo에서 값을 추출
+
     setError(""); //error message초기화
 
     if (isLoading || name === "" || email === "" || password === "") return; //loading중이거나, name or email or password 비어있으면, 함수 종료
@@ -76,7 +72,7 @@ export default function CreateAccount() {
         <Input
           onChange={onChange}
           name="name"
-          value={name}
+          value={userInfo.name} // userInfo 객체에서 값을 참조
           placeholder="Name"
           type="name"
           required
@@ -84,7 +80,7 @@ export default function CreateAccount() {
         <Input
           onChange={onChange}
           name="email"
-          value={email}
+          value={userInfo.email} // userInfo 객체에서 값을 참조
           placeholder="Email"
           type="email"
           required
@@ -92,7 +88,7 @@ export default function CreateAccount() {
         <Input
           onChange={onChange}
           name="password"
-          value={password}
+          value={userInfo.password} // userInfo 객체에서 값을 참조
           placeholder="Password"
           type="password"
           required
