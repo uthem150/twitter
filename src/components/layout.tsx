b896581f235309b12c7cf0ba40c461249bd39f54
uthem150 <uthem150@gmail.com>
2024-06-16 13:14:09 +0900

    Style. 북마크 라우트 버튼 푸터로 이동

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -159,6 +159,26 @@ export default function Layout() {
               </svg>
             </MenuItem>
           </Link>
+          {/* 북마크 목록 버튼 */}
+          <Link to={`/bookmark`}>
+            <MenuItem>
+              <svg
+                data-slot="icon"
+                fill="white"
+                strokeWidth="1.5"
+                stroke="white"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
+                ></path>
+              </svg>
+            </MenuItem>
+          </Link>
           {/* 프로필 연결 버튼 */}
           <Link to={`/profile/${userId}`}>
             <MenuItem>
