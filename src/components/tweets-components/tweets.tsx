04278faa03b1a107aa811b45c9f031582192c2e0
uthem150 <uthem150@gmail.com>
2024-06-21 23:54:16 +0900

    FIX. strokeLine 경고

--- src/components/tweets-components/tweets.tsx
+++ src/components/tweets-components/tweets.tsx
@@ -310,8 +310,8 @@ export default function Tweet({ photo, tweet, userId, id, createdAt }: ITweet) {
                 aria-hidden="true"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"
                 ></path>
               </svg>
